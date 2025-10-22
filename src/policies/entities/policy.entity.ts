393bc84b27091c230f4af677a3b260480fc742ae
Teo Lunas <lunasteobaldo@gmail.com>
2025-10-22 00:17:18 -0300

    Se crea entidad para las polizas

--- src/policies/entities/policy.entity.ts
+++ src/policies/entities/policy.entity.ts
@@ -1,5 +1,23 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity('policies')
 export class Policy {
 
-    
+    @PrimaryColumn('uuid')
+    id: string;
+
+    @Column('text')
+    rutTitular: string;
+
+    fechaEmision: Date;
+
+    @Column('text')
+    planSalud: string;
+
+    @Column('int')
+    prima: number;
+
+    @Column('text')
+    estado: string;
 
 }

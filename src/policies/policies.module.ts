bd82a3e10b66cdd77adf100f65e0563582a1e6ef
Teo Lunas <lunasteobaldo@gmail.com>
2025-10-22 01:41:39 -0300

    Se disponibilisa identidad de polizas

--- src/policies/policies.module.ts
+++ src/policies/policies.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PoliciesService } from './policies.service';
 import { PoliciesController } from './policies.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Policy } from './entities/policy.entity';
 
 @Module({
   controllers: [PoliciesController],
   providers: [PoliciesService],
+  imports: [
+    TypeOrmModule.forFeature([Policy])
+  ]
 })
 export class PoliciesModule {}

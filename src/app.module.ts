a00c74fb920bb1f247ca2831f2066512450a8d6e
Teo Lunas <lunasteobaldo@gmail.com>
2025-10-22 00:10:42 -0300

    Se configura conexion a al base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { PoliciesModule } from './policies/policies.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.BD_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities:true,
+    }),
     PoliciesModule
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+  

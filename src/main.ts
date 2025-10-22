5ab4bb2986b7abbe974995e1fad858c4d9df3af7
Teo Lunas <lunasteobaldo@gmail.com>
2025-10-22 02:14:40 -0300

    Configuracion swagger: - Se instala paquete de swagger - Se configura ruta y detalles de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Gestion de Polizas')
+    .setDescription('Api para la gestion de polizas de seguros')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }

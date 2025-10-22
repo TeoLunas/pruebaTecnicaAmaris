29ed02b390082627e4b965b880eef179c56b5868
Teo Lunas <lunasteobaldo@gmail.com>
2025-10-22 03:18:36 -0300

    Se crea dto: - Se crean campos para recibir en los request - Se documenta tipos  y ejemplos para swagger

--- src/policies/dto/create-policy.dto.ts
+++ src/policies/dto/create-policy.dto.ts
@@ -1,1 +1,41 @@
-export class CreatePolicyDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreatePolicyDto {
+
+    @ApiProperty({
+        example: '12345678-9'
+    })
+    @IsString()
+    @IsNotEmpty()
+    rutTitular: string;
+
+    @ApiProperty({
+        example: '01-01-2025'
+    })
+    @IsString()
+    @IsNotEmpty()
+    fechaEmision: Date;
+
+    @ApiProperty({
+        example: 'isapre'
+    })
+    @IsString()
+    @IsNotEmpty()
+    planSalud: string;
+
+    @ApiProperty({
+        example: "345122356"
+    })
+    @IsString()
+    @IsNotEmpty()
+    prima: number;
+
+    @ApiProperty({
+        example: "emitida"
+    })
+    @IsString()
+    @IsOptional()
+    estado: string;
+
+}
